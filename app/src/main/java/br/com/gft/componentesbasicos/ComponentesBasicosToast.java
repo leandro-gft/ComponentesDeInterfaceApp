01429412b2263635afe13cd79e42e1ed0057170c
leandro-gft <lpsacchi@gmail.com>
2020-05-27 09:20:36 -0300

    Criação do projeto e adição de tooglebutton, checkbox e switch

---
+++ app/src/main/java/br/com/gft/componentesbasicos/ComponentesBasicosToast.java
@@ -0,0 +1,14 @@
+package br.com.gft.componentesbasicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComponentesBasicosToast extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_componentes_basicos_toast);
+    }
+}
---
+++ app/src/main/java/br/com/gft/componentesbasicos/MainActivity.java
@@ -0,0 +1,47 @@
+package br.com.gft.componentesbasicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Switch switchLembrarSenha;
+    private ToggleButton toggleButton;
+    private TextView tvResultado;
+    private CheckBox checkBoxLembrarSenha;
+    private Button btEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        switchLembrarSenha = findViewById(R.id.switchLembrarSenha);
+        toggleButton = findViewById(R.id.toggleButton);
+        tvResultado = findViewById(R.id.tvResultado);
+        btEnviar = findViewById(R.id.btEnviar);
+        checkBoxLembrarSenha = findViewById(R.id.checkBoxLembrarSenha);
+    }
+
+    public void enviar(View view) {
+        if (switchLembrarSenha.isChecked()){
+            tvResultado.setText("Switch está ativado");
+        }
+        if (toggleButton.isChecked()){
+            tvResultado.setText("ToggleButton está LIGADO");
+        }
+        if (checkBoxLembrarSenha.isChecked()){
+            tvResultado.setText("Checkbox está ativada");
+        }
+
+
+    }
+}
